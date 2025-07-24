3daa8a66def11a2cc7106475c402f41617a962b9
drytebyte <d.rytebyte@gmail.com>
2025-07-24 23:31:10 +0000

    Initial commit of eCommerce website

---
+++ models/Product.js
@@ -0,0 +1,1 @@
+const mongoose = require("mongoose"); const ProductSchema = new mongoose.Schema({ name: String }); module.exports = mongoose.models.Product || mongoose.model("Product", ProductSchema);
