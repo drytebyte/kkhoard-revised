231904715fda446236eda10aa433d4bcdfbc4f6d
drytebyte <d.rytebyte@gmail.com>
2025-07-26 21:50:14 +0000

    Delete pages/api/seed/clear.js

--- pages/api/seed/clear.js
+++
@@ -1,9 +0,0 @@
-import dbConnect from '../../../lib/dbConnect'
-import Product from '../../../models/Product'
-
-export default async function handler(req, res) {
-  await dbConnect();
-  await Product.deleteMany({});
-  res.status(200).json({ message: 'Database cleared' });
-}
-
