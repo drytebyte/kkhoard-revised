d01c3698246aab14f2427361d9e655b2d4b6e680
drytebyte <d.rytebyte@gmail.com>
2025-07-26 16:51:54 +0000

    Update clear.js

--- pages/api/seed/clear.js
+++ pages/api/seed/clear.js
@@ -1,5 +1,5 @@
-import dbConnect from '../../../lib/dbConnect';
-import Product from '../../../models/Product';
+import dbConnect from "../../../lib/dbConnect";
+import Product from "../../../models/Product";
 
 export default async function handler(req, res) {
   await dbConnect();
